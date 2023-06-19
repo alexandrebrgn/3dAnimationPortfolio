86cc57c3bc32410f3afe631e9751791d00bb2b53
Alexandre BOURGUIGNON <a.bourguignon@pcadev02.atool-dev.fr>
2023-06-19 16:31:24 +0200

    website test

--- main.js
+++ main.js
@@ -3,26 +3,30 @@ import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 import { OrbitControls } from '/node_modules/three/examples/jsm/controls/OrbitControls'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
-let carMesh;
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
-
 renderer.setPixelRatio(window.devicePixelRatio); // Pixel de l'instanciation = pixels de l'ecran
 renderer.setSize(window.innerWidth, window.innerHeight); // Mettre en full-screen
 camera.position.setZ(30);
-
 renderer.render(scene, camera); // Render == draw
 
-const torus = new THREE.Mesh(new THREE.TorusGeometry(10, 3, 1000, 1000), new THREE.MeshStandardMaterial({color: 0xff6347}));
+// _______________________________Models____________________________________//
 
-//scene.add(torus)
+const torus = new THREE.Mesh(new THREE.TorusGeometry(10, 3, 1000, 1000), new THREE.MeshStandardMaterial({color: 0xff6347}));
+scene.add(torus)
+
+//_______________________________ LIGHTS ____________________________________//
 
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(15, 15, 15);
+
+// const directionalLight = new THREE.DirectionalLight(0xffffff,20);
+// directionalLight.position.set(0,1,0);
+// directionalLight.castShadow = true;
+// scene.add(directionalLight);
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
@@ -33,7 +37,9 @@ scene.add(lightHelper, gridHelper);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
-function animate() {
+// _______________________________FUNCTIONS ______________________________//
+
+function animate() { // Function to animate torus
   requestAnimationFrame(animate);
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.0005;
@@ -43,20 +49,21 @@ function animate() {
   renderer.render(scene, camera);
 }
 
-function loadGLTF() {
+function loadGLTF() { // Function to load gltf file of car
   const carMesh = new GLTFLoader();
 
-  carMesh.load('/model/lambo/scene.gltf', (gltf) => {
+  carMesh.load('/model/f50/scene.gltf', (gltf) => {
     gltf.scene.scale.set(10, 10, 10);
     scene.add(gltf.scene);
-    carMesh.position.x = 0;
-    carMesh.position.y = 0;
-    carMesh.position.z = 0;
+    carMesh.position.x = 10;
+    carMesh.position.y = 10;
+    carMesh.position.z = 10;
+    renderer.render(scene, camera);
   })
 }
 
 
-function addStar() {
+function addStar() { // Function to add stars
   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
   const material = new THREE.MeshStandardMaterial({color: 0xffffff});
   const star = new THREE.Mesh(geometry, material);
@@ -71,4 +78,4 @@ function addStar() {
 
 Array(200).fill().forEach(addStar)
 animate()
-loadGLTF()
+//loadGLTF()

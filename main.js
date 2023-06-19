c486d94b2598ce5bec20c75b2bf66b77b7bf2367
Alexandre BOURGUIGNON <a.bourguignon@pcadev02.atool-dev.fr>
2023-06-19 17:08:54 +0200

    camera changes, light changes

--- main.js
+++ main.js
@@ -4,15 +4,22 @@ import { OrbitControls } from '/node_modules/three/examples/jsm/controls/OrbitCo
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
 renderer.setPixelRatio(window.devicePixelRatio); // Pixel de l'instanciation = pixels de l'ecran
 renderer.setSize(window.innerWidth, window.innerHeight); // Mettre en full-screen
-camera.position.setZ(30);
+camera.position.x = 50;
+camera.position.y = 25;
+camera.position.z = 60;
+camera.rotation.y = 45/180*Math.PI;
 renderer.render(scene, camera); // Render == draw
 
+// const geometry = new THREE.PlaneGeometry( 1, 1 );
+// const material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+// const plane = new THREE.Mesh( geometry, material );
+// scene.add( plane );
 // _______________________________Models____________________________________//
 
 const torus = new THREE.Mesh(new THREE.TorusGeometry(10, 3, 1000, 1000), new THREE.MeshStandardMaterial({color: 0xff6347}));
@@ -23,17 +30,42 @@ const torus = new THREE.Mesh(new THREE.TorusGeometry(10, 3, 1000, 1000), new THR
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(15, 15, 15);
 
-// const directionalLight = new THREE.DirectionalLight(0xffffff,20);
-// directionalLight.position.set(0,1,0);
-// directionalLight.castShadow = true;
-// scene.add(directionalLight);
-
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
 const lightHelper = new THREE.PointLightHelper(pointLight);
 const gridHelper =new THREE.GridHelper(200, 50)
 scene.add(lightHelper, gridHelper);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff,2, 2);
+directionalLight.position.set(0,1,0);
+directionalLight.castShadow = false;
+scene.add(directionalLight);
+
+const light = new THREE.PointLight(0xc4c4c4, 5);
+const lighthelp = new THREE.PointLightHelper(light);
+light.position.set(0, 300, 500);
+scene.add(light, lighthelp);
+
+const light2 = new THREE.PointLight(0xc4c4c4, 5);
+const lighthelp2 = new THREE.PointLightHelper(light2);
+light.position.set(500, 100, 0);
+scene.add(light2, lighthelp2);
+
+const light3 = new THREE.PointLight(0xc4c4c4, 5);
+const lighthelp3 = new THREE.PointLightHelper(light3);
+light.position.set(0, 100, -500);
+scene.add(light3, lighthelp3);
+
+const light4 = new THREE.PointLight(0xc4c4c4, 5);
+const lighthelp4 = new THREE.PointLightHelper(light4);
+light.position.set(-500, 300, 0);
+scene.add(light4, lighthelp4);
+
+const light5 = new THREE.PointLight(0xc4c4c4, 5);
+const lighthelp5 = new THREE.PointLightHelper(light5);
+light.position.set(0, 300, 500);
+scene.add(light5, lighthelp5);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 

91313e6cd6b826d7cd8d990533404e80029bbc6c
Alexandre BOURGUIGNON <a.bourguignon@pcadev02.atool-dev.fr>
2023-06-20 10:38:42 +0200

    torus test

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ renderer.render(scene, camera); // Render == draw
 // _______________________________Models____________________________________//
 
 const torus = new THREE.Mesh(new THREE.TorusGeometry(10, 3, 1000, 1000), new THREE.MeshStandardMaterial({color: 0xff6347}));
-// scene.add(torus)
+scene.add(torus)
 
 //_______________________________ LIGHTS ____________________________________//
 

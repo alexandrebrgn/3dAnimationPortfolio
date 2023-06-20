f553fde763dbdff5828f1dca1998188d3ffb3f50
Alexandre BOURGUIGNON <a.bourguignon@pcadev02.atool-dev.fr>
2023-06-20 10:39:54 +0200

    test f40

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ renderer.render(scene, camera); // Render == draw
 // _______________________________Models____________________________________//
 
 const torus = new THREE.Mesh(new THREE.TorusGeometry(10, 3, 1000, 1000), new THREE.MeshStandardMaterial({color: 0xff6347}));
-scene.add(torus)
+// scene.add(torus)
 
 //_______________________________ LIGHTS ____________________________________//
 
@@ -105,4 +105,4 @@ function addStar() { // Function to add stars
 
 Array(200).fill().forEach(addStar)
 animate()
-//loadGLTF()
+loadGLTF()
